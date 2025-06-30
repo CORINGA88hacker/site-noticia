f6822c1b5b53aad57e13a0cbd28bcfbc431bef3e
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-06-29 23:46:44 -0300

    Update and rename mangas.json to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHIc2E4XwRO5FXo4uHlTQVRArOis73MjE",
+  authDomain: "projeto-deus-yato-928.firebaseapp.com",
+  databaseURL: "https://projeto-deus-yato-928-sk-default-rtdb.firebaseio.com/",
+  projectId: "projeto-deus-yato-928",
+  storageBucket: "projeto-deus-yato-928.appspot.com",
+  messagingSenderId: "790408726854",
+  appId: "1:790408726854:android:e2f0de7b7d5dba96b0fd47"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
