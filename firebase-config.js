52f2e7ad514272be760834b8fa09c83f6ffd8575
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-06-30 00:36:07 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-
+// Configuração Firebase do seu projeto
 const firebaseConfig = {
   apiKey: "AIzaSyBHIc2E4XwRO5FXo4uHlTQVRArOis73MjE",
-  authDomain: "projeto-deus-yato-928.firebaseapp.com",
-  databaseURL: "https://projeto-deus-yato-928-sk-default-rtdb.firebaseio.com/",
-  projectId: "projeto-deus-yato-928",
-  storageBucket: "projeto-deus-yato-928.appspot.com",
+  authDomain: "projeto-deus-yato-928-sk-default-rtdb.firebaseapp.com",
+  databaseURL: "https://projeto-deus-yato-928-sk-default-rtdb.firebaseio.com",
+  projectId: "projeto-deus-yato-928-sk-default-rtdb",
+  storageBucket: "projeto-deus-yato-928-sk-default-rtdb.appspot.com",
   messagingSenderId: "790408726854",
   appId: "1:790408726854:android:e2f0de7b7d5dba96b0fd47"
 };
 
 firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
